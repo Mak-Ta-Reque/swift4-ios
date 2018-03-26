4a51ac41499f8bfa21666eb499cb102d25ea5193
Mak-Ta-Reque <maktareq@gmail.come>
2018-03-26 21:34:12 +0200

    Practicing chapter one

---
+++ TutorialOne/TutorialOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialOne
+//
+//  Created by Md Abdul Kadir on 26.03.18.
+//  Copyright © 2018 Md Abdul Kadir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
